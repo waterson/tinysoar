ea616de243f95318147a065c93d256bfaf971e80
waterson <waterson>
2001-07-04 19:51:20 +0000

    Add a newline after the assertion text.

--- debug.c
+++ debug.c
@@ -13,6 +13,9 @@ runtime_assert(const char* fmtstr, ...)
     vfprintf(stderr, fmtstr, ap);
     va_end(ap);
 
+    if (fmtstr[strlen(fmtstr) - 1] != '\n')
+        fprintf(stderr, "\n");
+
 #if defined(_MSC_VER) && _M_IX86 >= 300
     __asm { int 3 };
 #elif defined(__GNUC__) && defined(__i386)
