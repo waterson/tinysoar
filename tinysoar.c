d719f1f181cb32abcfb7a2b7e6f90de9b77b359c
waterson <waterson>
2002-06-07 05:22:26 +0000

    Implement basic rule printing.

--- tinysoar.c
+++ tinysoar.c
@@ -41,6 +41,7 @@
 #include "soar.h"
 #include "symtab.h"
 #include "parser.h"
+#include "rete.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
@@ -291,17 +292,381 @@ preferences -r <pref>";
     return TCL_OK;
 }
 
+#ifdef DEBUG
+/*
+ * Find the pnode corresponding to the rule in the network.
+ */
+static struct beta_node *
+find_rule(struct beta_node *node, const char *name)
+{
+    if (node->type == beta_node_type_production
+        && strcmp(node->data.production->name, name) == 0) {
+        return node;
+    }
 
+    for ( ; node != 0; node = node->siblings) {
+        if (node->children) {
+            struct beta_node *result =
+                find_rule(node->children, name);
+
+            if (result)
+                return result;
+        }
+    }
+
+    return 0;
+}
+#endif
+
+/*
+ * Given the specified `relative' variable binding, return the
+ * variable symbol that is bound. A nil symbol (i.e., zero) is
+ * returned if there is no binding.
+ */
+static symbol_t
+find_variable_for(struct variable_binding_list *bindings,
+                  variable_binding_t            binding,
+                  int                           depth)
+{
+    int absolute_depth = depth - GET_VARIABLE_BINDING_DEPTH(binding);
+    SET_VARIABLE_BINDING_DEPTH(binding, absolute_depth);
+    for ( ; bindings != 0; bindings = bindings->next) {
+        if (VARIABLE_BINDINGS_ARE_EQUAL(bindings->binding, binding))
+            return bindings->variable;
+    }
+
+    return 0;
+}
+
+/*
+ * Given the specified `relative' variable binding, return the
+ * variable symbol that is bound. A new variable symbol is created and
+ * assigned to the binding, if necessary.
+ */
+static symbol_t
+ensure_variable_for(struct variable_binding_list **bindings,
+                    variable_binding_t             binding,
+                    int                            depth)
+{
+    symbol_t result;
+
+    if (SYMBOL_IS_NIL(result = find_variable_for(*bindings, binding, depth))) {
+        struct variable_binding_list *entry =
+            (struct variable_binding_list *) malloc(sizeof(struct variable_binding_list));
+
+        int absolute_depth = depth - GET_VARIABLE_BINDING_DEPTH(binding);
+        SET_VARIABLE_BINDING_DEPTH(binding, absolute_depth);
+
+        entry->binding = binding;
+        INIT_SYMBOL(result, symbol_type_variable,
+                    *bindings ? (GET_SYMBOL_VALUE((*bindings)->variable) + 1) : 1);
+        entry->variable = result;
+        entry->next = *bindings;
+        *bindings = entry;
+    }
+
+    return result;
+}
+
+/*
+ * Print the LHS tests associated with the specified beta node and
+ * field.
+ */
+static void
+print_tests(char                         **result,
+            size_t                        *sz,
+            struct agent                  *agent,
+            struct variable_binding_list **bindings,
+            int                            depth,
+            struct beta_node              *node,
+            field_t                        field)
+{
+    struct beta_test *test;
+    symbol_t variable = 0;
+    symbol_t constant = 0;
+    bool_t goal = 0;
+    bool_t acceptable = 0;
+
+    /* Look for a constant binding in the alpha network. */
+    if (node->alpha_node) {
+        switch (field) {
+        case field_id:    constant = node->alpha_node->id;    break;
+        case field_attr:  constant = node->alpha_node->attr;  break;
+        case field_value: constant = node->alpha_node->value; break;
+        default:
+            ERROR(("unexpected field type"));
+        }
+
+        /* Determine if the alpha node tests an acceptable preference. */
+        if (field == field_value) {
+            int i;
+
+            for (i = 8; !acceptable && i < 16; ++i) {
+                struct alpha_node *alpha;
+                for (alpha = agent->alpha_nodes[i]; alpha != 0; alpha = alpha->siblings) {
+                    if (alpha == node->alpha_node) {
+                        acceptable = 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /* Look for a variable binding. */
+    for (test = node->data.tests; test != 0; test = test->next) {
+        if (test->field == field) {
+            switch (test->type) {
+            case test_type_goal_id: {
+                /* XXX this is hokey. Why don't goal tests work like
+                   other tests? */
+                variable_binding_t binding;
+
+                ASSERT(field == field_id, ("goal test on non-id field"));
+                goal = 1;
+
+                INIT_VARIABLE_BINDING(binding, field_id, 0); 
+                variable = ensure_variable_for(bindings, binding, depth);
+                break;
+            }
+
+            case test_type_conjunctive:
+                ERROR(("unexpected test type"));
+                /* Fall through... */
+            case test_type_disjunctive:
+                break;
+
+            default:
+                ASSERT(test->relational_type == relational_type_variable,
+                       ("expected constant test to be in alpha node"));
+
+                variable = ensure_variable_for(bindings, test->data.variable_referent, depth);
+                break;
+            }
+        }
+    }
+
+    /* Now see if it's a disjunctive test. */
+    for (test = node->data.tests; test != 0; test = test->next) {
+        /* All the disjuncts will test the same field. */
+        if (test->type == test_type_disjunctive && test->field == field) {
+            struct beta_test *disjunct;
+
+            vsmcatf(result, sz, "{");
+
+            /* Degenerate, but possible. */
+            if (goal)
+                vsmcatf(result, sz, " state");
+
+            vsmcatf(result, sz, " <<");
+
+            if (variable)
+                vsmcatf(result, sz, " <v%d>", GET_SYMBOL_VALUE(variable));
+
+            for (disjunct = test->data.disjuncts; disjunct != 0; disjunct = disjunct->next) {
+                ASSERT(disjunct->relational_type == relational_type_constant,
+                       ("expected constant value in disjunct"));
+
+                vsmcatf(result, sz, " %s",
+                        symtab_find_name(&symtab,
+                                         disjunct->data.constant_referent));
+            }
+
+            /* This would be really degenerate, but it's possible. */
+            if (constant)
+                vsmcatf(result, sz, " %s", symtab_find_name(&symtab, constant));
+
+            vsmcatf(result, sz, " >> }");
+
+            if (acceptable)
+                vsmcatf(result, sz, " +");
+
+            return;
+        }
+    }
+
+    if (variable && constant) {
+        vsmcatf(result, sz, "{ %s<v%d> %s }",
+                (goal ? "state " : ""), /* Degenerate, but possible. */
+                GET_SYMBOL_VALUE(variable),
+                symtab_find_name(&symtab, constant));
+    }
+    else {
+        if (goal)
+            vsmcatf(result, sz, "state ");
+
+        if (constant)
+            vsmcatf(result, sz, "%s", symtab_find_name(&symtab, constant));
+        else {
+            if (!variable && field == field_value) {
+                variable_binding_t binding;
+                INIT_VARIABLE_BINDING(binding, field_value, 0);
+                variable = ensure_variable_for(bindings, binding, depth);
+            }
+
+            vsmcatf(result, sz, "<v%d>", GET_SYMBOL_VALUE(variable));
+        }
+    }
+
+    if (acceptable)
+        vsmcatf(result, sz, " +");
+}
+
+/*
+ * Print the specified RHS value.
+ */
+static void
+print_rhs_value(char                         **result,
+                size_t                        *sz,
+                struct variable_binding_list  *bindings,
+                int                            depth,
+                struct rhs_value              *value)
+{
+    switch (value->type) {
+    case rhs_value_type_symbol:
+        vsmcatf(result, sz, "%s", symtab_find_name(&symtab, value->val.symbol));
+        break;
+
+    case rhs_value_type_variable_binding:
+        {
+            symbol_t var =
+                find_variable_for(bindings, value->val.variable_binding, depth);
+
+            vsmcatf(result, sz, "<v%d>", GET_SYMBOL_VALUE(var));
+        }
+
+        break;
+
+    case rhs_value_type_unbound_variable:
+        {
+            int num = bindings ? GET_SYMBOL_VALUE(bindings->variable) : 0;
+            vsmcatf(result, sz, "<v%d>", num + value->val.unbound_variable + 1);
+        }
+        break; 
+    }
+}
+
+/*
+ * Print the specified rule.
+ */
+struct beta_node_list {
+    struct beta_node      *node;
+    struct beta_node_list *next;
+};
+
+static bool_t
+print_rule(char **result, size_t *sz, struct agent *agent, const char *name)
+{
+    struct beta_node *pnode = 0;
+
+#ifdef DEBUG
+    if (agent->root_node)
+        pnode = find_rule(agent->root_node, name);
+#endif
+
+    if (pnode) {
+        struct variable_binding_list *bindings = 0;
+        struct beta_node_list *nodes;
+        struct beta_node *node;
+        struct action *action;
+        int depth;
+
+        vsmcatf(result, sz, "%s", name);
+
+        /* Collect the nodes into a list so we can walk the list in
+           reverse. (We could do this by reversing pointers, but I'm just
+           not that desparate to make this space-efficient.) */
+        nodes = 0;
+        for (node = pnode; node != 0; node = node->parent) {
+            struct beta_node_list *entry;
+
+            switch (node->type) {
+            case beta_node_type_positive_join:
+            case beta_node_type_negative:
+                entry = (struct beta_node_list *) malloc(sizeof(struct beta_node_list));
+                entry->node = node;
+                entry->next = nodes;
+                nodes = entry;
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        /* Print the rule's conditions. */
+        depth = 0;
+        while (nodes) {
+            struct beta_node_list *next = nodes->next;
+
+            vsmcatf(result, sz, "\n(");
+
+            print_tests(result, sz, agent, &bindings, depth, nodes->node, field_id);
+
+            /* XXX Hacky, but it works. */
+            vsmcatf(result, sz, " %s^",
+                    (nodes->node->type == beta_node_type_negative) ? "-" : "");
+
+            print_tests(result, sz, agent, &bindings, depth, nodes->node, field_attr);
+
+            vsmcatf(result, sz, " ");
+
+            print_tests(result, sz, agent, &bindings, depth, nodes->node, field_value);
+
+            vsmcatf(result, sz, ")");
+
+            free(nodes);
+            nodes = next;
+            ++depth;
+        }
+
+        vsmcatf(result, sz, "\n-->");
+
+        /* Print the rule's actions. */
+        --depth;
+        for (action = pnode->data.production->actions; action != 0; action = action->next) {
+            vsmcatf(result, sz, "\n(");
+            print_rhs_value(result, sz, bindings, depth, &action->id);
+            vsmcatf(result, sz, " ^");
+            print_rhs_value(result, sz, bindings, depth, &action->attr);
+            vsmcatf(result, sz, " ");
+            print_rhs_value(result, sz, bindings, depth, &action->value);
+
+            /* XXX don't use `debug' function. */
+            vsmcatf(result, sz, " %s", debug_preference_type_to_string(action->preference_type));
+            if (action->preference_type & preference_type_binary) {
+                vsmcatf(result, sz, " ");
+                print_rhs_value(result, sz, bindings, depth, &action->referent);
+            }
+
+            vsmcatf(result, sz, ")");
+        }
+
+        /* Clean up the bound variables. */
+        while (bindings) {
+            struct variable_binding_list *doomed = bindings;
+            bindings = bindings->next;
+            free(doomed);
+        }
+
+        /* Let the caller know we found the rule. */
+        return 1;
+    }
+
+    /* No such rule. */
+    return 0;
+}
+
+/*
+ * wme enumerator callback for `print': if this is the id we wanna print,
+ * then this'll dump the dope.
+ */
 struct print_closure {
     char     **result;
     size_t    *sz;
     symbol_t   id;
 };
 
-/*
- * wme enumerator callback for `print': if this is the id we wanna print,
- * then this'll dump the dope.
- */
 static void
 print_enumerator(struct agent *agent, struct wme *wme, void *closure)
 {
@@ -353,7 +718,7 @@ static char *print_result;
 
     /* Sanity check the command. */
     if (argc < 2) {
-        interp->result = "print [-stack | <id>]";
+        interp->result = "print [-stack | <id> | <rule>]";
         return TCL_ERROR;
     }
 
@@ -375,11 +740,30 @@ static char *print_result;
     pc.result = &print_result;
     pc.sz = &print_result_sz;
 
-    while (i < argc) {
-        INIT_SYMBOL(pc.id, symbol_type_identifier, atoi(argv[i++]));
+    for ( ; i < argc; ++i) {
+        int id = atoi(argv[i]);
+        if (id) {
+            if (print_result)
+                vsmcatf(&print_result, &print_result_sz, "\n");
 
-        vsmcatf(&print_result, &print_result_sz, "[%d]", GET_SYMBOL_VALUE(pc.id));
-        wmem_enumerate_wmes(&agent, print_enumerator, &pc);
+            INIT_SYMBOL(pc.id, symbol_type_identifier, atoi(argv[i]));
+            vsmcatf(&print_result, &print_result_sz, "[%d]", GET_SYMBOL_VALUE(pc.id));
+            wmem_enumerate_wmes(&agent, print_enumerator, &pc);
+        }
+        else {
+            if (! print_rule(&print_result, &print_result_sz, &agent, argv[i])) {
+                if (print_result) {
+                    free(print_result);
+                    print_result = 0;
+                }
+
+                vsmcatf(&print_result, &print_result_sz,
+                        "no such rule, `%s'", argv[i]);
+
+                interp->result = print_result;
+                return TCL_ERROR;
+            }
+        }
     }
 
     interp->result = print_result;
@@ -424,6 +808,9 @@ __declspec(dllexport)
 int
 Tinysoar_Init(Tcl_Interp *interp)
 {
+    if (! Tcl_InitStubs(interp, TCL_VERSION, 1))
+        return TCL_ERROR;
+
     Tcl_PkgProvide(interp, "TinySoar", "1.0");
 
     agent_init(&agent);
@@ -439,3 +826,10 @@ Tinysoar_Init(Tcl_Interp *interp)
 
     return TCL_OK;
 }
+
+int
+Tinysoar_SafeInit(Tcl_Interp *interp)
+{
+    return Tinysoar_Init(interp);
+}
+
