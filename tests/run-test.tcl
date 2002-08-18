75f2f59a7b1ffa3d21d3ce5d48c6ef4b77243f68
waterson <waterson>
2002-08-18 05:56:48 +0000

    Add post-conditions to the test to verify that chunks are created correctly. Allow for arbitrary number of cycles so that we can verify that chunks get created and work right.

--- tests/run-test.tcl
+++ tests/run-test.tcl
@@ -7,20 +7,27 @@ if {$argc == 0 || $argc > 1} {
 
 set output_link 4
 set cycles 0
+set post ""
 source $argv
 
-if {$cycles > 0} {
-    while {[incr cycles -1] >= 0} {
+foreach count $cycles {
+    init-soar
+
+    while {[incr count -1] >= 0} {
         elaborate
     }
+}
 
-    # Look for `^success t' on the top state.
-    array set attrs [lreplace [split [print $output_link]] 0 0]
-    set success [lindex [array get attrs ^success] 1]
+# Look for `^success t' on the top state.
+array set attrs [lreplace [split [print $output_link]] 0 0]
+set success [lindex [array get attrs ^success] 1]
 
-    if {[string compare $success "t"] == 0} {
-        exit 0
+if {[string compare $success "t"] == 0} {
+    if {"$post" != ""} {
+        exit [eval $post]
     }
+
+    exit 0
 }
 
 exit 1
